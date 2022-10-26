aad8575dd4c875e0d52d556146855e67bf81fc88
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2022-10-26 11:33:33 +0100

    Added responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
+    navbar.classList.toggle("open-menu");
     menu.classList.toggle("move");
+}
+
+window.onscroll = () => {
+    navbar.classList.remove("open-menu");
+    menu.classList.remove("move");
 }
 
 // Review Swiper
